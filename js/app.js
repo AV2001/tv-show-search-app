083d864b246658a16a0511efc630842a8ed42fec
Anirudh Vadlamani <anirudhvadlamani2001@gmail.com>
2022-12-01 22:13:18 +0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// ********************
+// DOM SELECTORS
+// ********************
+const searchForm = document.querySelector('.search-form');
+const searchQuery = document.getElementById('search-query');
+const searchBtn = document.querySelector('button');
+const sectionShows = document.querySelector('.section-shows');
